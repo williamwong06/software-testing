e308d793f04a2965fd1ef6f1c3ddb96f2d5fa921
williamwong12 <43776756+williamwong12@users.noreply.github.com>
2021-10-03 07:19:03 +0200

    update-logboek

---
+++ Week2/API/EigenAPIProductsGet.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-type: application/json');
+$conn = new mysqli('ID328986_Webshop.db.webhosting.be', 'ID328986_Webshop', 'azerty123', 'ID328986_Webshop');
+$data = array();
+
+$sql = "SELECT * FROM Products";
+$result = $conn
+->query($sql);
+while($row = $result
+->fetch_assoc()) {
+    $data[] = $row;
+}
+echo json_encode($data);
+exit(0);
+?>
