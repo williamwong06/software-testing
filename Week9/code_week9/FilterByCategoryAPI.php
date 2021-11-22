e7e52fbc47fc36584a0af07321d123285e5b754b
williamwong12 <43776756+williamwong12@users.noreply.github.com>
2021-11-22 06:23:54 +0100

    update

--- Week9/code_week9/FilterByCategoryAPI.php
+++ Week9/code_week9/FilterByCategoryAPI.php
@@ -4,23 +4,12 @@ $conn = new mysqli('ID328986_webShop.db.webhosting.be', 'ID328986_webShop', 'aze
 $data = array();
 
 
-if (isset($_GET['accessoires'])) {
-    $sql = "SELECT * FROM Products WHERE Category_Name = accessoires" ;
-    $result = $conn->query($sql);
-    $data = $result->fetch_assoc();
-}
-
-else if (isset($_GET['keuken'])) {
-        $sql = "SELECT * FROM Products WHERE Category_Name = keuken" ;
-        $result = $conn->query($sql);
-        $data = $result->fetch_assoc();
-    }
-}
-
-else if (isset($_GET['GSSM'])) {
-    $sql = "SELECT * FROM Products WHERE Category_Name = GSM" ;
-    $result = $conn->query($sql);
-    $data = $result->fetch_assoc();
+if (isset($_GET['category'])) {
+    $sql = "SELECT * FROM Products WHERE Category_Name ='" . $_GET['category'] . "'";
+    $result = $conn ->query($sql);
+    while($row = $result
+    ->fetch_assoc()) {
+        $data[] = $row;
     }
 }
 
@@ -36,19 +25,4 @@ else {
 echo json_encode($data);
 exit(0);
 
-
-
-
-
-
-
-$sql = "SELECT * FROM Products where";
-$result = $conn
-->query($sql);
-while($row = $result
-->fetch_assoc()) {
-    $data[] = $row;
-}
-echo json_encode($data);
-exit(0);
 ?>
